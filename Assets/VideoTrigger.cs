ff50adcae7845472678f55ace73721ee2134446d
Ross Ning <252551516@qq.com>
2017-02-05 17:47:01 -0500

    Added fade in and out effect when switching between two cameras.

--- Assets/VideoTrigger.cs
+++ Assets/VideoTrigger.cs
@@ -4,35 +4,132 @@ using UnityEngine;
 using RenderHeads.Media.AVProVideo;
 
 public class VideoTrigger : MonoBehaviour {
+	private float fadeTextureAlpha = 0.0f;
+	public float fadeSpeed = 2.0f;
+
+	private enum Status
+	{
+		InMainCamera,
+		MainCameraFadeOut,
+		VideoCameraFadeIn,
+		InVideoCamera,
+		VideoCameraFadeOut,
+		MainCameraFadeIn
+	};
+	private Status status = Status.InMainCamera;
+	private bool areaEntered = false;
+	private bool areaExited = false;
+
+	public Texture fadeTexture;
+
+
+
+	void FadeIn() {
+		fadeTextureAlpha -= Mathf.Clamp01(Time.deltaTime * fadeSpeed);
+	}
+
+	void FadeOut() {
+		fadeTextureAlpha += Mathf.Clamp01(Time.deltaTime * fadeSpeed);
+	}
 
 	// Use this for initialization
 	void Start () {
 		
 	}
+
+	void OnGUI() {
+
+		if (status == Status.InMainCamera || status == Status.InVideoCamera)
+			return;
+
+		GUI.color = new Color(fadeTextureAlpha, fadeTextureAlpha, fadeTextureAlpha, fadeTextureAlpha);
+		GUI.DrawTexture( new Rect(0, 0, Screen.width, Screen.height ), fadeTexture );
 	
-	// Update is called once per frame
-	void Update () {
-		
 	}
 
-	void OnTriggerEnter(Collider other) {
-		// Destroy(gameObject);
-		// MediaPlayer mediaPlayer = (MediaPlayer)FindObjectOfType(typeof(MediaPlayer));
-		// mediaPlayer.Stop ();
-
-		Camera mainCamera = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera>();
-		Camera vidCamera = GameObject.FindGameObjectWithTag ("360VideoCamera").GetComponent<Camera>();
-	
+	static void SwitchToVideoCamera ()
+	{
+		Camera mainCamera = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera> ();
+		Camera vidCamera = GameObject.FindGameObjectWithTag ("360VideoCamera").GetComponent<Camera> ();
 		mainCamera.enabled = false;
 		vidCamera.enabled = true;
 	}
 
-	void OnTriggerExit(Collider other) {
-
-		Camera mainCamera = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera>();
-		Camera vidCamera = GameObject.FindGameObjectWithTag ("360VideoCamera").GetComponent<Camera>();
-
+	static void SwitchToMainCamera ()
+	{
+		Camera mainCamera = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera> ();
+		Camera vidCamera = GameObject.FindGameObjectWithTag ("360VideoCamera").GetComponent<Camera> ();
 		mainCamera.enabled = true;
 		vidCamera.enabled = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (status == Status.InMainCamera) {
+			
+			if (areaEntered) {
+				areaEntered = false;
+				status = Status.MainCameraFadeOut;
+			}
+
+		} else if (status == Status.MainCameraFadeOut) {
+			
+			if (fadeTextureAlpha >= 1.0f) {
+				SwitchToVideoCamera ();
+				status = Status.VideoCameraFadeIn;
+			} else {
+				FadeOut ();
+			}
+
+		} else if (status == Status.VideoCameraFadeIn) {
+			
+			if (fadeTextureAlpha <= 0.0f) {
+				status = Status.InVideoCamera;
+			} else {
+				FadeIn ();
+			}
+
+		} else if (status == Status.InVideoCamera) {
+
+			if (areaExited) {
+				areaExited = false;
+				status = Status.VideoCameraFadeOut;
+			}
+
+		} else if (status == Status.VideoCameraFadeOut) {
+
+			if (fadeTextureAlpha >= 1.0f) {
+				SwitchToMainCamera ();
+				status = Status.MainCameraFadeIn;
+			} else {
+				FadeOut ();
+			}
+
+		} else if (status == Status.MainCameraFadeIn) {
+
+			if (fadeTextureAlpha <= 0.0f) {
+				status = Status.InMainCamera;
+			} else {
+				FadeIn ();
+			}
+
+		}
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		// Destroy(gameObject);
+		// MediaPlayer mediaPlayer = (MediaPlayer)FindObjectOfType(typeof(MediaPlayer));
+		// mediaPlayer.Stop ();
+
+		areaEntered = true;
+
+	}
+
+	void OnTriggerExit(Collider other) {
+
+		areaExited = true;
+
 	}
 }
