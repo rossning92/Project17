13d9478b552142a18d4bd6c30f3b1a5ae579fae5
Ross Ning <252551516@qq.com>
2017-02-17 15:24:28 -0500

    We can set audio reverb effect transition curve: linear, exponential, sigmoid, logarithmic

--- Assets/AudioReverbFor360Video.cs
+++ Assets/AudioReverbFor360Video.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class AudioReverbFor360Video : MonoBehaviour {
 
+
+	public enum ReverbTransitionCurve
+	{
+		Linear, Logarithmic, Exponential, Sigmoid
+	}
+
+
 	public AudioReverbFilter audioReverb0Degree;
 	public AudioReverbFilter audioReverb180Degree;
+	public ReverbTransitionCurve reverbTransitionCurve = ReverbTransitionCurve.Linear;
 
 	// Use this for initialization
 	void Start () {
@@ -29,24 +37,36 @@ public class AudioReverbFor360Video : MonoBehaviour {
 			t /= 180.0f;
 		
 
-			AudioReverbFilter f = DemoApp.Instance.videoSphereReverbFilter;
+			if (reverbTransitionCurve == ReverbTransitionCurve.Exponential) {
+				t = Mathf.Sin (1.5f * Mathf.PI + 0.5f * Mathf.PI * t) + 1.0f;
+			} else if (reverbTransitionCurve == ReverbTransitionCurve.Logarithmic) {
+				t = Mathf.Sin (0.5f * Mathf.PI * t);
+			} else if (reverbTransitionCurve == ReverbTransitionCurve.Sigmoid) {
+				t = 0.5f * Mathf.Cos (Mathf.PI + Mathf.PI * t) + 0.5f;
+			}
 
-			f.decayHFRatio = Mathf.Lerp (audioReverb0Degree.decayHFRatio, audioReverb180Degree.decayHFRatio, t);
-			f.decayTime = Mathf.Lerp (audioReverb0Degree.decayTime, audioReverb180Degree.decayTime, t);
-			f.density = Mathf.Lerp (audioReverb0Degree.density, audioReverb180Degree.density, t);
-			f.diffusion = Mathf.Lerp (audioReverb0Degree.diffusion, audioReverb180Degree.diffusion, t);
-			f.dryLevel = Mathf.Lerp (audioReverb0Degree.dryLevel, audioReverb180Degree.dryLevel, t);
-			f.hfReference = Mathf.Lerp (audioReverb0Degree.hfReference, audioReverb180Degree.hfReference, t);
-			f.lfReference = Mathf.Lerp (audioReverb0Degree.lfReference, audioReverb180Degree.lfReference, t);
-			f.reflectionsDelay = Mathf.Lerp (audioReverb0Degree.reflectionsDelay, audioReverb180Degree.reflectionsDelay, t);
-			f.reflectionsLevel = Mathf.Lerp (audioReverb0Degree.reflectionsLevel, audioReverb180Degree.reflectionsLevel, t);
-			f.reverbDelay = Mathf.Lerp (audioReverb0Degree.reverbDelay, audioReverb180Degree.reverbDelay, t);
-			f.reverbLevel = Mathf.Lerp (audioReverb0Degree.reverbLevel, audioReverb180Degree.reverbLevel, t);
-			f.room = Mathf.Lerp (audioReverb0Degree.room, audioReverb180Degree.room, t);
-			f.roomHF = Mathf.Lerp (audioReverb0Degree.roomHF, audioReverb180Degree.roomHF, t);
-			f.roomLF = Mathf.Lerp (audioReverb0Degree.roomLF, audioReverb180Degree.roomLF, t);
-			f.roomRolloff = Mathf.Lerp (audioReverb0Degree.roomRolloff, audioReverb180Degree.roomRolloff, t);
+
+			LerpReverbEffect (DemoApp.Instance.videoSphereReverbFilter, audioReverb0Degree, audioReverb180Degree, t);
 
 		}
+	}
+
+	void LerpReverbEffect (AudioReverbFilter target, AudioReverbFilter a, AudioReverbFilter b, float t)
+	{
+		target.decayHFRatio = Mathf.Lerp (a.decayHFRatio, b.decayHFRatio, t);
+		target.decayTime = Mathf.Lerp (a.decayTime, b.decayTime, t);
+		target.density = Mathf.Lerp (a.density, b.density, t);
+		target.diffusion = Mathf.Lerp (a.diffusion, b.diffusion, t);
+		target.dryLevel = Mathf.Lerp (a.dryLevel, b.dryLevel, t);
+		target.hfReference = Mathf.Lerp (a.hfReference, b.hfReference, t);
+		target.lfReference = Mathf.Lerp (a.lfReference, b.lfReference, t);
+		target.reflectionsDelay = Mathf.Lerp (a.reflectionsDelay, b.reflectionsDelay, t);
+		target.reflectionsLevel = Mathf.Lerp (a.reflectionsLevel, b.reflectionsLevel, t);
+		target.reverbDelay = Mathf.Lerp (a.reverbDelay, b.reverbDelay, t);
+		target.reverbLevel = Mathf.Lerp (a.reverbLevel, b.reverbLevel, t);
+		target.room = Mathf.Lerp (a.room, b.room, t);
+		target.roomHF = Mathf.Lerp (a.roomHF, b.roomHF, t);
+		target.roomLF = Mathf.Lerp (a.roomLF, b.roomLF, t);
+		target.roomRolloff = Mathf.Lerp (a.roomRolloff, b.roomRolloff, t);
 	}
 }
