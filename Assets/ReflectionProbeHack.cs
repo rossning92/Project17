a56ed9b3eb01096405e2be45aa5ebba09ed0fc2f
Ross Ning <rossning92@gmail.com>
2017-02-21 15:04:58 -0500

    Added script for delayed rendering reflection probe.

---
+++ Assets/ReflectionProbeHack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReflectionProbeHack : MonoBehaviour {
+
+	public float RenderReflectionProbeAfter = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		RenderReflectionProbeAfter -= Time.deltaTime;
+		if ( RenderReflectionProbeAfter < 0 )
+		{
+			GetComponent<ReflectionProbe> ().RenderProbe ();
+		}
+
+	}
+}
