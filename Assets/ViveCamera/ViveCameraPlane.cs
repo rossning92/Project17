cd8add6c779b277f985931101acd2068a9de4f50
Ross Ning <rossning92@gmail.com>
2017-02-21 17:22:35 -0500

    Crop the Vive camera image with respect to the ratio of the plane

--- Assets/ViveCamera/ViveCameraPlane.cs
+++ Assets/ViveCamera/ViveCameraPlane.cs
@@ -3,15 +3,14 @@ using UnityEngine;
 
 public class ViveCameraPlane : MonoBehaviour
 {
-	public bool undistorted = true;
-	public bool cropped = true;
+	public bool undistorted = false;
+	// public bool cropped = true;
 
-	private Material material;
+	public Material material;
 	private Transform target;
 
-	void OnStart()
+	void Start()
 	{
-		material = GetComponent<Material> ();
 		target = GetComponent<Transform> ();
 	}
 
@@ -60,22 +59,46 @@ public class ViveCameraPlane : MonoBehaviour
 		// The undistorted video feed has 'bad' areas near the edges where the original
 		// square texture feed is stretched to undo the fisheye from the lens.
 		// Therefore, you'll want to crop it to the specified frameBounds to remove this.
-		if (cropped)
-		{
-			var bounds = source.frameBounds;
-			material.mainTextureOffset = new Vector2(bounds.uMin, bounds.vMin);
 
-			var du = bounds.uMax - bounds.uMin;
-			var dv = bounds.vMax - bounds.vMin;
-			material.mainTextureScale = new Vector2(du, dv);
 
-			aspect *= Mathf.Abs(du / dv);
-        }
-		else
-		{
-			material.mainTextureOffset = Vector2.zero;
-			material.mainTextureScale = new Vector2(1, -1);
+
+		// Crop the image with respect to the ratio of the plane
+		float aspectCamera = 16f / 9f;
+		float aspectPlane = transform.localScale.x / transform.localScale.y;
+
+		float scaleX = 1, scaleY = 1;
+		float offsetX = 0, offsetY = 0;
+		if (aspectPlane < aspectCamera) {
+			scaleX = aspectPlane / aspectCamera;
+			offsetX = (1f - scaleX) * 0.5f;
+		} else {
+			scaleY = aspectCamera / aspectPlane;
+			offsetY = (1f - scaleY) * 0.5f;
 		}
+		material.mainTextureOffset = new Vector2(offsetX, -offsetY);
+		material.mainTextureScale = new Vector2(scaleX, -scaleY);
+
+
+
+
+		// if (cropped)
+		// {
+		// 	var bounds = source.frameBounds;
+		// 	material.mainTextureOffset = new Vector2(bounds.uMin, bounds.vMin);
+		// 
+		// 	var du = bounds.uMax - bounds.uMin;
+		// 	var dv = bounds.vMax - bounds.vMin;
+		// 	material.mainTextureScale = new Vector2(du, dv);
+		// 
+		// 	aspect *= Mathf.Abs(du / dv);
+        // }
+		// else
+		// {
+		// 	material.mainTextureOffset = Vector2.zero;
+		// 	material.mainTextureScale = new Vector2(1, -1);
+		// }
+
+
 
 		// target.localScale = new Vector3(1, 1.0f / aspect, 1);
 
